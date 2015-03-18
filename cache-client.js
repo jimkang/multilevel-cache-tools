48f11d08036c82fefe0ff2423775f99740563a0d
Jim Kang <jimkang@gmail.com>
2015-03-17 23:01:32 -0400

    Factored generic client stuff out of memoize.

--- cache-client.js
+++ cache-client.js
@@ -2,20 +2,15 @@ var levelCacheTools = require('level-cache-tools');
 var multilevel = require('multilevel');
 var net = require('net');
 
-function memoize(opts) {
-  var fn;
+function createConnectedClientDb(opts) {
   var port;
 
   if (opts) {
-    fn = opts.fn;
     port = opts.port;
   }
 
-  if (!fn) {
-    throw new Error('No fn provided to client.');
-  }
   if (!port) {
-    throw new Error('No port provided to client.')
+    throw new Error('No port provided to createConnectedClientDb.')
   }
 
   var cacheDb = multilevel.client();
@@ -27,6 +22,21 @@ function memoize(opts) {
   var rpcStream = cacheDb.createRpcStream();
   connection.pipe(rpcStream).pipe(connection);
 
+  return cacheDb;
+}
+
+function memoize(opts) {
+  var fn;
+
+  if (opts) {
+    fn = opts.fn;
+  }
+
+  if (!fn) {
+    throw new Error('No fn provided to client.');
+  }
+
+  var cacheDb = createConnectedClientDb(opts);
   return new levelCacheTools.MemoizeCache(cacheDb, opts.fn);
 }
 
